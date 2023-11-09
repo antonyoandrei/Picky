053706644a6e53728a99e10163265113a420e3d0
Antonyo Andrei <antonyoandrei7@gmail.com>
2023-11-09 12:22:57 +0100

    mongo db uri

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -4,14 +4,10 @@ import connect from './db/connect';
 
 const PORT = config.app.PORT;
 
-connect().then(
-    () => 
-        async function onServerStart() {
-            console.log('Connected to database!')
+connect().then(() => {
+    console.log('Connected to database!')
 
-            app.listen(PORT, () => {
-                console.log(`Server is running on port ${PORT}`)
-            });
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    });
 });
-
-
